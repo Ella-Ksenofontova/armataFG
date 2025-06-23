9d698c3f770d43ec2a7b1816418d7a2e96eb15e8
Ella Ksenofontova <ellaksenofontova@gmail.com>
2025-06-23 13:58:31 +0300

    Начальный коммит

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+import pkg from 'gulp'
+import ghPages from 'gulp-gh-pages';
+const { src, dest, parallel, series, watch } = pkg
+
+import browserSync from 'browser-sync'
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
+const sass = gulpSass(dartSass)
+import postCss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import concat from 'gulp-concat'
+import rename from 'gulp-rename'
+import uglify from 'gulp-uglify'
+import autoprefixer from 'autoprefixer'
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+	})
+}
+
+function scripts() {
+	return src([
+		'app/libs/modernizr/modernizr.js',
+		'app/libs/jquery/jquery-1.11.2.min.js',
+		'app/libs/waypoints/waypoints.min.js',
+		'app/libs/animate/animate-css.js',
+	])
+		.pipe(concat('libs.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src(['sass/**/*.sass'])
+		.pipe(sass({
+			'include css': true,
+			includePaths: ['app/libs/bourbon/core']
+		}))
+		.pipe(postCss([
+			autoprefixer({ grid: 'autoplace' }),
+			cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
+		]))
+		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function startwatch() {
+	watch(['sass/**/*.sass'], { usePolling: true }, styles)
+	watch(['app/js/common.js', 'app/libs/**/*.js'], { usePolling: true }, scripts)
+	watch(['app/*.html'], { usePolling: true }).on('change', browserSync.reload)
+}
+
+pkg.task('deploy', function() {
+    return pkg.src('./app/**/*')
+        .pipe(ghPages());
+});
+
+export { scripts, styles }
+export let assets = series(scripts, styles)
+
+export default series(scripts, styles, parallel(browsersync, startwatch))
